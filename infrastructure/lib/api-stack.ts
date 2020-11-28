90ec5876ca80df5ad2a12710c96781511ca2d063
David Andersson <nderssonpublic@gmail.com>
2020-11-28 20:40:17 +1100

    remove cognito for the moment

--- infrastructure/lib/api-stack.ts
+++ infrastructure/lib/api-stack.ts
@@ -96,27 +96,27 @@ export class ApiStack extends cdk.Stack {
     });
 
     // Add Cognito authorizer
-    const authorizer = new apigateway.CfnAuthorizer(this, 'Authorizer', {
-      restApiId: api.restApiId,
-      type: apigateway.AuthorizationType.COGNITO,
-      identitySource: apigateway.IdentitySource.header('Authorization'),
-      providerArns: [ENVIRONMENT.AWS_IDENTITY_PROVIDER_ARN],
-      name: 'PackageAuth',
-    });
+    // const authorizer = new apigateway.CfnAuthorizer(this, 'Authorizer', {
+    //   restApiId: api.restApiId,
+    //   type: apigateway.AuthorizationType.COGNITO,
+    //   identitySource: apigateway.IdentitySource.header('Authorization'),
+    //   providerArns: [ENVIRONMENT.AWS_IDENTITY_PROVIDER_ARN],
+    //   name: 'PackageAuth',
+    // });
 
-    // Protect resources with cognito
-    const versionResource = api.root.addResource(CONFIG.api.resources.version);
-    const specsResource = versionResource.addResource(
-      CONFIG.api.resources.specs.pathPart
-    );
-    specsResource.addMethod('PUT', integration, {
-      authorizationType: apigateway.AuthorizationType.COGNITO,
-      authorizationScopes:
-        CONFIG.api.resources.specs.methods.put.authorizationScopes,
-      authorizer: {
-        authorizerId: authorizer.logicalId,
-      },
-    });
+    // // Protect resources with cognito
+    // const versionResource = api.root.addResource(CONFIG.api.resources.version);
+    // const specsResource = versionResource.addResource(
+    //   CONFIG.api.resources.specs.pathPart
+    // );
+    // specsResource.addMethod('PUT', integration, {
+    //   authorizationType: apigateway.AuthorizationType.COGNITO,
+    //   authorizationScopes:
+    //     CONFIG.api.resources.specs.methods.put.authorizationScopes,
+    //   authorizer: {
+    //     authorizerId: authorizer.logicalId,
+    //   },
+    // });
 
     // DNS listing
     const zone = route53.PublicHostedZone.fromLookup(this, 'PublicHostedZone', {
