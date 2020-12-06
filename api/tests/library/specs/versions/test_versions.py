248b7665e0e605802b6e91b491b21cd9a587a71a
David Andersson <nderssonpublic@gmail.com>
2020-12-06 17:39:25 +1100

    add endpoint for updating a spec

--- api/tests/library/specs/versions/test_versions.py
+++ api/tests/library/specs/versions/test_versions.py
@@ -223,155 +223,165 @@ def test_put_version_mismatch_error(monkeypatch):
     assert version_2 in response.data.decode()
 
 
-# def test_put_too_many_models_error(monkeypatch, _clean_package_storage_table):
-#     """
-#     GIVEN body spec and spec id and user that already has too many models
-#     WHEN put is called with the body and spec id
-#     THEN a 402 with an invalid spec is returned.
-#     """
-#     mock_request = mock.MagicMock()
-#     mock_headers = {"X-LANGUAGE": "JSON"}
-#     mock_request.headers = mock_headers
-#     monkeypatch.setattr(server.Request, "request", mock_request)
-#     version = "version 1"
-#     schemas = {
-#         "Schema": {
-#             "type": "object",
-#             "x-tablename": "schema",
-#             "properties": {"id": {"type": "integer"}},
-#         }
-#     }
-#     body = json.dumps(
-#         {"info": {"version": version}, "components": {"schemas": schemas}}
-#     )
-#     spec_id = "id 1"
-#     user = "user 1"
-#     database.get_database().create_update_spec(
-#         sub=user, spec_id=spec_id, version="version 1", model_count=100
-#     )
+def test_put_too_many_models_error(monkeypatch, _clean_package_storage_table):
+    """
+    GIVEN body, spec id, version and user that already has too many models
+    WHEN put is called with the body and spec id
+    THEN a 402 with an invalid spec is returned.
+    """
+    mock_request = mock.MagicMock()
+    mock_headers = {"X-LANGUAGE": "JSON"}
+    mock_request.headers = mock_headers
+    monkeypatch.setattr(server.Request, "request", mock_request)
+    version = "version 1"
+    schemas = {
+        "Schema": {
+            "type": "object",
+            "x-tablename": "schema",
+            "properties": {"id": {"type": "integer"}},
+        }
+    }
+    body = json.dumps(
+        {"info": {"version": version}, "components": {"schemas": schemas}}
+    )
+    spec_id = "id 1"
+    user = "user 1"
+    database.get_database().create_update_spec(
+        sub=user, spec_id=spec_id, version="version 1", model_count=100
+    )
 
-#     response = specs.put(body=body.encode(), spec_id=spec_id, user=user)
+    response = versions.put(
+        body=body.encode(), spec_id=spec_id, version=version, user=user
+    )
 
-#     assert response.status_code == 402
-#     assert response.mimetype == "text/plain"
-#     assert "exceeded" in response.data.decode()
-#     assert ": 100," in response.data.decode()
-#     assert "spec: 1" in response.data.decode()
-
-
-# def test_put_database_count_error(monkeypatch, _clean_package_storage_table):
-#     """
-#     GIVEN body with invalid spec and spec id and database that raises DatabaseError
-#     WHEN put is called with the body and spec id
-#     THEN a 500 is returned.
-#     """
-#     mock_request = mock.MagicMock()
-#     mock_headers = {"X-LANGUAGE": "JSON"}
-#     mock_request.headers = mock_headers
-#     monkeypatch.setattr(server.Request, "request", mock_request)
-#     version = "version 1"
-#     schemas = {
-#         "Schema": {
-#             "type": "object",
-#             "x-tablename": "schema",
-#             "properties": {"id": {"type": "integer"}},
-#         }
-#     }
-#     body = json.dumps(
-#         {"info": {"version": version}, "components": {"schemas": schemas}}
-#     )
-#     spec_id = "id 1"
-#     user = "user 1"
-#     mock_database_count_customer_models = mock.MagicMock()
-#     mock_database_count_customer_models.side_effect = database.exceptions.DatabaseError
-#     monkeypatch.setattr(
-#         database.get_database(),
-#         "count_customer_models",
-#         mock_database_count_customer_models,
-#     )
-
-#     response = specs.put(body=body.encode(), spec_id=spec_id, user=user)
-
-#     assert response.status_code == 500
-#     assert response.mimetype == "text/plain"
-#     assert "database" in response.data.decode()
+    assert response.status_code == 402
+    assert response.mimetype == "text/plain"
+    assert "exceeded" in response.data.decode()
+    assert ": 100," in response.data.decode()
+    assert "spec: 1" in response.data.decode()
 
 
-# def test_put_storage_error(monkeypatch, _clean_package_storage_table):
-#     """
-#     GIVEN body with invalid spec and spec id
-#     WHEN put is called with the body and spec id
-#     THEN a 400 with an invalid spec is returned.
-#     """
-#     mock_request = mock.MagicMock()
-#     mock_headers = {"X-LANGUAGE": "JSON"}
-#     mock_request.headers = mock_headers
-#     monkeypatch.setattr(server.Request, "request", mock_request)
-#     version = "version 1"
-#     schemas = {
-#         "Schema": {
-#             "type": "object",
-#             "x-tablename": "schema",
-#             "properties": {"id": {"type": "integer"}},
-#         }
-#     }
-#     body = json.dumps(
-#         {"info": {"version": version}, "components": {"schemas": schemas}}
-#     )
-#     spec_id = "id 1"
-#     user = "user 1"
-#     mock_storage_create_update_spec = mock.MagicMock()
-#     mock_storage_create_update_spec.side_effect = storage.exceptions.StorageError
-#     monkeypatch.setattr(
-#         storage.get_storage_facade(),
-#         "create_update_spec",
-#         mock_storage_create_update_spec,
-#     )
+def test_put_database_count_error(monkeypatch, _clean_package_storage_table):
+    """
+    GIVEN body with invalid spec and spec id and database that raises DatabaseError
+    WHEN put is called with the body and spec id
+    THEN a 500 is returned.
+    """
+    mock_request = mock.MagicMock()
+    mock_headers = {"X-LANGUAGE": "JSON"}
+    mock_request.headers = mock_headers
+    monkeypatch.setattr(server.Request, "request", mock_request)
+    version = "version 1"
+    schemas = {
+        "Schema": {
+            "type": "object",
+            "x-tablename": "schema",
+            "properties": {"id": {"type": "integer"}},
+        }
+    }
+    body = json.dumps(
+        {"info": {"version": version}, "components": {"schemas": schemas}}
+    )
+    spec_id = "id 1"
+    user = "user 1"
+    mock_database_check_would_exceed_free_tier = mock.MagicMock()
+    mock_database_check_would_exceed_free_tier.side_effect = (
+        database.exceptions.DatabaseError
+    )
+    monkeypatch.setattr(
+        database.get_database(),
+        "check_would_exceed_free_tier",
+        mock_database_check_would_exceed_free_tier,
+    )
 
-#     response = specs.put(body=body.encode(), spec_id=spec_id, user=user)
+    response = versions.put(
+        body=body.encode(), spec_id=spec_id, version=version, user=user
+    )
 
-#     assert response.status_code == 500
-#     assert response.mimetype == "text/plain"
-#     assert "storing" in response.data.decode()
+    assert response.status_code == 500
+    assert response.mimetype == "text/plain"
+    assert "database" in response.data.decode()
 
 
-# def test_put_database_update_error(monkeypatch, _clean_package_storage_table):
-#     """
-#     GIVEN body and spec id
-#     WHEN put is called with the body and spec id
-#     THEN the spec is stored against the spec id.
-#     """
-#     mock_request = mock.MagicMock()
-#     mock_headers = {"X-LANGUAGE": "JSON"}
-#     mock_request.headers = mock_headers
-#     monkeypatch.setattr(server.Request, "request", mock_request)
-#     version = "version 1"
-#     schemas = {
-#         "Schema": {
-#             "type": "object",
-#             "x-tablename": "schema",
-#             "properties": {"id": {"type": "integer"}},
-#         }
-#     }
-#     body = json.dumps(
-#         {"info": {"version": version}, "components": {"schemas": schemas}}
-#     )
-#     spec_id = "id 1"
-#     user = "user 1"
-#     mock_database_create_update_spec = mock.MagicMock()
-#     mock_database_create_update_spec.side_effect = database.exceptions.DatabaseError
-#     monkeypatch.setattr(
-#         database.get_database(),
-#         "create_update_spec",
-#         mock_database_create_update_spec,
-#     )
+def test_put_storage_error(monkeypatch, _clean_package_storage_table):
+    """
+    GIVEN body with invalid spec and spec id
+    WHEN put is called with the body and spec id
+    THEN a 400 with an invalid spec is returned.
+    """
+    mock_request = mock.MagicMock()
+    mock_headers = {"X-LANGUAGE": "JSON"}
+    mock_request.headers = mock_headers
+    monkeypatch.setattr(server.Request, "request", mock_request)
+    version = "version 1"
+    schemas = {
+        "Schema": {
+            "type": "object",
+            "x-tablename": "schema",
+            "properties": {"id": {"type": "integer"}},
+        }
+    }
+    body = json.dumps(
+        {"info": {"version": version}, "components": {"schemas": schemas}}
+    )
+    spec_id = "id 1"
+    user = "user 1"
+    mock_storage_create_update_spec = mock.MagicMock()
+    mock_storage_create_update_spec.side_effect = storage.exceptions.StorageError
+    monkeypatch.setattr(
+        storage.get_storage_facade(),
+        "create_update_spec",
+        mock_storage_create_update_spec,
+    )
 
-#     response = specs.put(body=body.encode(), spec_id=spec_id, user=user)
+    response = versions.put(
+        body=body.encode(), spec_id=spec_id, version=version, user=user
+    )
 
-#     assert storage.get_storage_facade().get_spec(
-#         user=user, spec_id=spec_id, version=version
-#     ) == json.dumps({"components": {"schemas": schemas}}, separators=(",", ":"))
-#     assert database.get_database().count_customer_models(sub=user) == 0
-#     assert response.status_code == 500
-#     assert response.mimetype == "text/plain"
-#     assert "database" in response.data.decode()
+    assert response.status_code == 500
+    assert response.mimetype == "text/plain"
+    assert "storing" in response.data.decode()
+
+
+def test_put_database_update_error(monkeypatch, _clean_package_storage_table):
+    """
+    GIVEN body and spec id
+    WHEN put is called with the body and spec id
+    THEN the spec is stored against the spec id.
+    """
+    mock_request = mock.MagicMock()
+    mock_headers = {"X-LANGUAGE": "JSON"}
+    mock_request.headers = mock_headers
+    monkeypatch.setattr(server.Request, "request", mock_request)
+    version = "version 1"
+    schemas = {
+        "Schema": {
+            "type": "object",
+            "x-tablename": "schema",
+            "properties": {"id": {"type": "integer"}},
+        }
+    }
+    body = json.dumps(
+        {"info": {"version": version}, "components": {"schemas": schemas}}
+    )
+    spec_id = "id 1"
+    user = "user 1"
+    mock_database_create_update_spec = mock.MagicMock()
+    mock_database_create_update_spec.side_effect = database.exceptions.DatabaseError
+    monkeypatch.setattr(
+        database.get_database(),
+        "create_update_spec",
+        mock_database_create_update_spec,
+    )
+
+    response = versions.put(
+        body=body.encode(), spec_id=spec_id, version=version, user=user
+    )
+
+    assert storage.get_storage_facade().get_spec(
+        user=user, spec_id=spec_id, version=version
+    ) == json.dumps({"components": {"schemas": schemas}}, separators=(",", ":"))
+    assert database.get_database().count_customer_models(sub=user) == 0
+    assert response.status_code == 500
+    assert response.mimetype == "text/plain"
+    assert "database" in response.data.decode()
