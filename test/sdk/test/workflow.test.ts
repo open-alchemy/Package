b1c95f24d91bc8910d5ee5197e0e5815e53f9c63
David Andersson <jdkandersson@@users.noreply.github.com>
2020-12-28 11:24:46 +1030

    move access token retrieval to helpers package

--- test/sdk/test/workflow.test.ts
+++ test/sdk/test/workflow.test.ts
@@ -1,11 +1,9 @@
-import assert from 'assert';
-
-import AWS from 'aws-sdk';
 import {
   SpecsService,
   SpecService,
   SpecError,
 } from '@open-alchemy/package-sdk';
+import { getAccessToken } from 'helpers';
 
 const VERSION = 'version 1';
 const TITLE = 'title 1';
@@ -35,52 +33,7 @@ describe('create spec', () => {
   let specService: SpecService;
 
   beforeAll(async () => {
-    const awsRegion = process.env['AWS_DEFAULT_REGION'] || null;
-    assert.ok(
-      typeof awsRegion === 'string',
-      'AWS_DEFAULT_REGION environment variable not defined'
-    );
-
-    const userPoolId = process.env['USER_POOL_ID'] || null;
-    assert.ok(
-      typeof userPoolId === 'string',
-      'USER_POOL_ID environment variable not defined'
-    );
-
-    const clientId = process.env['CLIENT_ID'] || null;
-    assert.ok(
-      typeof clientId === 'string',
-      'CLIENT_ID environment variable not defined'
-    );
-
-    const testUsername = process.env['TEST_USERNAME'] || null;
-    assert.ok(
-      typeof testUsername === 'string',
-      'TEST_USERNAME environment variable not defined'
-    );
-
-    const testPassword = process.env['TEST_PASSWORD'] || null;
-    assert.ok(
-      typeof testPassword === 'string',
-      'TEST_PASSWORD environment variable not defined'
-    );
-
-    const authFlow = 'ADMIN_USER_PASSWORD_AUTH';
-
-    const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider(
-      { region: awsRegion }
-    );
-    const response = await cognitoIdentityServiceProvider
-      .adminInitiateAuth({
-        AuthFlow: authFlow,
-        ClientId: clientId,
-        UserPoolId: userPoolId,
-        AuthParameters: { USERNAME: testUsername, PASSWORD: testPassword },
-      })
-      .promise();
-    assert.ok(response.AuthenticationResult !== undefined);
-    assert.ok(response.AuthenticationResult.AccessToken !== undefined);
-    accessToken = response.AuthenticationResult?.AccessToken;
+    accessToken = await getAccessToken();
   });
 
   beforeEach(() => {
