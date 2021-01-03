fb651a63e7195cfa75223ad54f9a5849fe405515
David Andersson <nderssonpublic@gmail.com>
2021-01-03 14:39:58 +1100

    change to no root install

--- security/tests/conftest.py
+++ security/tests/conftest.py
@@ -2,6 +2,10 @@
 
 import pytest
 
+pytest_plugins = (  # pylint: disable=invalid-name
+    "open_alchemy.package_security.pytest_plugin"
+)
+
 
 @pytest.fixture(scope="session", autouse=True)
 def use_service_secret(_service_secret):
