0b8234c1621f7788e978edafb3b80ef06db3323c
David Andersson <nderssonpublic@gmail.com>
2021-01-04 18:14:31 +1100

    add parsing of authroization header

--- index/library/__init__.py
+++ index/library/__init__.py
@@ -1,1 +1,55 @@
 """Library for the index application."""
+
+import base64
+import binascii
+import dataclasses
+
+from . import exceptions
+
+
+@dataclasses.dataclass
+class Authorization:
+    """Authorization information."""
+
+    public_key: str
+    secret_key: str
+
+
+def parse_authorization_header(value: str) -> Authorization:
+    """
+    Parse the authorization header.
+
+    Args:
+        value: The value of the authorization header (in the form 'Basic <base 64
+            encoded>').
+
+    Returns:
+        The public and secret key from the header.
+
+    """
+    expected_start = "Basic "
+    if not value.startswith(expected_start):
+        raise exceptions.UnauthorizedError(
+            f'Authorization header does not start with "{expected_start}", {value=}'
+        )
+
+    encoded_token_str = value[len(expected_start) :]
+    try:
+        encoded_token_bytes = encoded_token_str.encode()
+        decoded_token = base64.b64decode(encoded_token_bytes).decode()
+    except ValueError as exc:
+        raise exceptions.UnauthorizedError(
+            f"could not decode token, {encoded_token_str=}, {value=}"
+        ) from exc
+
+    if decoded_token.count(":") != 1:
+        raise exceptions.UnauthorizedError(
+            f"decoded token has multiple values, {decoded_token=}, {value=}"
+        )
+    public_key, secret_key = decoded_token.split(":")
+    if len(public_key) == 0:
+        raise exceptions.UnauthorizedError(f"public key empty, {public_key=}, {value=}")
+    if len(secret_key) == 0:
+        raise exceptions.UnauthorizedError(f"secret key empty, {secret_key=}, {value=}")
+
+    return Authorization(public_key=public_key, secret_key=secret_key)
--- infrastructure/lib/database-stack.ts
+++ infrastructure/lib/database-stack.ts
@@ -13,25 +13,6 @@ export class DatabaseStack extends cdk.Stack {
     const sub = { name: 'sub', type: dynamodb.AttributeType.STRING };
 
     // Database for the specs
-    const tableReplicationRegions = [
-      // 'ap-south-1',
-      // 'eu-north-1',
-      // 'eu-west-3',
-      // 'eu-south-1',
-      // 'eu-west-2',
-      // 'eu-west-1',
-      // 'ap-northeast-2',
-      // 'ap-northeast-1',
-      // 'ca-central-1',
-      // 'sa-east-1',
-      // 'ap-southeast-1',
-      'ap-southeast-2',
-      // 'eu-central-1',
-      // 'us-east-1',
-      // 'us-east-2',
-      // 'us-west-1',
-      // 'us-west-2',
-    ].filter((region) => region != ENVIRONMENT.AWS_DEFAULT_REGION);
     const specsTable = new dynamodb.Table(this, 'SpecsTable', {
       partitionKey: { ...sub },
       tableName: CONFIG.database.spec.tableName,
@@ -40,8 +21,6 @@ export class DatabaseStack extends cdk.Stack {
         type: dynamodb.AttributeType.STRING,
       },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      replicationRegions: tableReplicationRegions,
-      stream: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES,
     });
     specsTable.addLocalSecondaryIndex({
       indexName: CONFIG.database.spec.localSecondaryIndexName,
@@ -61,8 +40,6 @@ export class DatabaseStack extends cdk.Stack {
         type: dynamodb.AttributeType.STRING,
       },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      replicationRegions: tableReplicationRegions,
-      stream: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES,
     });
     credentialsTable.addGlobalSecondaryIndex({
       indexName: CONFIG.database.credentials.globalSecondaryIndexName,
