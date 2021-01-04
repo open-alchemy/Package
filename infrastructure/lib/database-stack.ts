ad2d6a4774f19c47d377e995d6db793ad71e9a29
David Andersson <nderssonpublic@gmail.com>
2021-01-04 16:28:00 +1100

    correct the replication regions to those that are supported

--- infrastructure/lib/database-stack.ts
+++ infrastructure/lib/database-stack.ts
@@ -13,9 +13,25 @@ export class DatabaseStack extends cdk.Stack {
     const sub = { name: 'sub', type: dynamodb.AttributeType.STRING };
 
     // Database for the specs
-    const tableReplicationRegions = regionInfo.RegionInfo.regions
-      .map((region) => region.name)
-      .filter((region) => region != ENVIRONMENT.AWS_DEFAULT_REGION);
+    const tableReplicationRegions = [
+      'ap-south-1',
+      'eu-north-1',
+      'eu-west-3',
+      'eu-south-1',
+      'eu-west-2',
+      'eu-west-1',
+      'ap-northeast-2',
+      'ap-northeast-1',
+      'ca-central-1',
+      'sa-east-1',
+      'ap-southeast-1',
+      'ap-southeast-2',
+      'eu-central-1',
+      'us-east-1',
+      'us-east-2',
+      'us-west-1',
+      'us-west-2',
+    ].filter((region) => region != ENVIRONMENT.AWS_DEFAULT_REGION);
     const specsTable = new dynamodb.Table(this, 'SpecsTable', {
       partitionKey: { ...sub },
       tableName: CONFIG.database.spec.tableName,
