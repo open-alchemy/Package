1a5563ce3a24a0aa15f0b04cc27c710c7ee650b2
David Andersson <nderssonpublic@gmail.com>
2021-01-05 21:25:04 +1100

    remove unnecessary items

--- web/src/app/components/specs-create-button/specs-refresh-create.component.spec.ts
+++ web/src/app/components/specs-create-button/specs-refresh-create.component.spec.ts
@@ -2,14 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MatIconModule } from '@angular/material/icon';
 
-import { TestScheduler } from 'rxjs/testing';
-
 import { SpecsCreateButtonComponent } from './specs-create-button.component';
 
 describe('SpecsCreateButtonComponent', () => {
   let component: SpecsCreateButtonComponent;
   let fixture: ComponentFixture<SpecsCreateButtonComponent>;
-  let testScheduler: TestScheduler;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -19,10 +16,6 @@ describe('SpecsCreateButtonComponent', () => {
 
     fixture = TestBed.createComponent(SpecsCreateButtonComponent);
     component = fixture.componentInstance;
-
-    testScheduler = new TestScheduler((actual, expected) => {
-      expect(actual).toEqual(expected);
-    });
   });
 
   it('should create', () => {
