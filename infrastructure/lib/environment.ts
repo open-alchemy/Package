01713867f377aa2a2dddf2e2676f3bbd9912865e
David Andersson <nderssonpublic@gmail.com>
2021-01-07 14:33:35 +1100

    simplify environment class

--- infrastructure/lib/environment.ts
+++ infrastructure/lib/environment.ts
@@ -22,76 +22,57 @@ class Environment {
   private _awsIdentityProviderArn: IAwsIdentityProviderArn | null = null;
   private _alarmEmailAddress: IAlarmEmailAddress | null = null;
 
-  get stack(): IStack {
-    if (this._stack === null) {
-      const stack = process.env[STACK_KEY];
+  private getValue(value: string | null, envKey: string): string {
+    if (value === null) {
+      const envValue = process.env[envKey];
       assert.ok(
-        typeof stack === 'string',
+        typeof envValue === 'string',
         `${STACK_KEY} missing or not a string`
       );
-      this._stack = stack;
+      value = envValue;
     }
+    return value;
+  }
+
+  get stack(): IStack {
+    this._stack = this.getValue(this._stack, STACK_KEY);
     return this._stack;
   }
 
   get awsAccount(): IAwsAccount {
-    if (this._awsAccount === null) {
-      const awsAccount = process.env[AWS_ACCOUNT_KEY];
-      assert.ok(
-        typeof awsAccount === 'string',
-        `${AWS_ACCOUNT_KEY} missing or not a string`
-      );
-      this._awsAccount = awsAccount;
-    }
+    this._awsAccount = this.getValue(this._awsAccount, AWS_ACCOUNT_KEY);
     return this._awsAccount;
   }
 
   get awsDefaultRegion(): IAwsDefaultRegion {
-    if (this._awsDefaultRegion === null) {
-      const awsDefaultRegion = process.env[AWS_DEFAULT_REGION_KEY];
-      assert.ok(
-        typeof awsDefaultRegion === 'string',
-        `${AWS_DEFAULT_REGION_KEY} missing or not a string`
-      );
-      this._awsDefaultRegion = awsDefaultRegion;
-    }
+    this._awsDefaultRegion = this.getValue(
+      this._awsDefaultRegion,
+      AWS_DEFAULT_REGION_KEY
+    );
     return this._awsDefaultRegion;
   }
 
   get awsOpenAlchemyCertificateArn(): IAwsOpenAlchemyCertificateArn {
-    if (this._awsOpenAlchemyCertificateArn === null) {
-      const awsOpenAlchemyCertificateArn =
-        process.env[AWS_OPEN_ALCHEMY_CERTIFICATE_ARN_KEY];
-      assert.ok(
-        typeof awsOpenAlchemyCertificateArn === 'string',
-        `${AWS_OPEN_ALCHEMY_CERTIFICATE_ARN_KEY} missing or not a string`
-      );
-      this._awsOpenAlchemyCertificateArn = awsOpenAlchemyCertificateArn;
-    }
+    this._awsOpenAlchemyCertificateArn = this.getValue(
+      this._awsOpenAlchemyCertificateArn,
+      AWS_OPEN_ALCHEMY_CERTIFICATE_ARN_KEY
+    );
     return this._awsOpenAlchemyCertificateArn;
   }
 
   get awsIdentityProviderArn(): IAwsIdentityProviderArn {
-    if (this._awsIdentityProviderArn === null) {
-      const awsIdentityProviderArn = process.env[AWS_IDENTITY_PROVIDER_ARN_KEY];
-      assert.ok(
-        typeof awsIdentityProviderArn === 'string',
-        `${AWS_IDENTITY_PROVIDER_ARN_KEY} missing or not a string`
-      );
-      this._awsIdentityProviderArn = awsIdentityProviderArn;
-    }
+    this._awsIdentityProviderArn = this.getValue(
+      this._awsIdentityProviderArn,
+      AWS_IDENTITY_PROVIDER_ARN_KEY
+    );
     return this._awsIdentityProviderArn;
   }
 
   get alarmEmailAddress(): IAlarmEmailAddress {
-    if (this._alarmEmailAddress === null) {
-      const alarmEmailAddress = process.env[ALARM_EMAIL_ADDRESS_KEY];
-      assert.ok(
-        typeof alarmEmailAddress === 'string',
-        `${ALARM_EMAIL_ADDRESS_KEY} missing or not a string`
-      );
-      this._alarmEmailAddress = alarmEmailAddress;
-    }
+    this._alarmEmailAddress = this.getValue(
+      this._alarmEmailAddress,
+      ALARM_EMAIL_ADDRESS_KEY
+    );
     return this._alarmEmailAddress;
   }
 }
